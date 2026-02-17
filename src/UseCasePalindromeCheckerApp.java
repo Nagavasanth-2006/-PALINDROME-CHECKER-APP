fcef24daeb948c74e83596862779e747ccb82a7b
Nagavasanth-2006 <nagavasanthpuli@gmail.com>
2026-02-17 15:47:46 +0530

    UC1: Add welcome message module

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,4 +1,4 @@
-public class UseCasePalidromeCheckerApp {
+public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Palindrome Checker App");
