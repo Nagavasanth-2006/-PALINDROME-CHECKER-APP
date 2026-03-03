66dac6d25a970ba1dbe479393c3fd4e2e6f439f0
Nagavasanth-20006 <nagavasanthpuli@gmail.com>
2026-03-03 16:05:03 +0530

    Cleaned up project files

--- -PALINDROME-CHECKER-APP/src/UseCasePalindromeCheckerApp.java
+++ -PALINDROME-CHECKER-APP/src/UseCasePalindromeCheckerApp.java
@@ -8,13 +8,10 @@ class StackStrategy implements PalindromeStrategy {
     @Override
     public boolean check(String input) {
         if (input == null) return false;
-
         Stack<Character> stack = new Stack<>();
-
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 return false;
@@ -27,12 +24,16 @@ class StackStrategy implements PalindromeStrategy {
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "level";
-
         PalindromeStrategy strategy = new StackStrategy();
 
+        long startTime = System.nanoTime();
         boolean isPalindrome = strategy.check(input);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + duration + " ns");
     }
 }
