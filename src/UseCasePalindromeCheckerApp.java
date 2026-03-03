ebb3ce5268679883a5dd10490a712ce990273358
Nagavasanth-2006 <nagavasanthpuli@gmail.com>
2026-03-03 16:17:24 +0530

    Add palindrome checking using stack strategy
    Implemented PalindromeStrategy interface and StackStrategy class for palindrome checking.

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,4 +1,40 @@
+import java.util.Stack;
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        if (input == null) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 public class UseCasePalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "level";
+        PalindromeStrategy strategy = new StackStrategy();
+
+        long startTime = System.nanoTime();
+        boolean isPalindrome = strategy.check(input);
+        long endTime = System.nanoTime();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
+    }
+}public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
         String input = "A man a plan a canal Panama";
