f9d4b3c9c1159b35ea7652c8a055f0f88025db5c
Nagavasanth-2006 <nagavasanthpuli@gmail.com>
2026-03-03 15:34:22 +0530

    Refactor palindrome checker to use user input

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,22 +1,8 @@
+import java.util.Scanner;
+
 public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        String input = "A man a plan a canal Panama";
-
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        boolean isPalindrome = true;
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
-=======
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input : ");
         String input = scanner.nextLine();
@@ -26,7 +12,6 @@ public class UseCasePalindromeCheckerApp {
 
         System.out.println("Is Palindrome? : " + result);
         scanner.close();
->>>>>>> f184864 (Base application setup)
     }
 }
 
